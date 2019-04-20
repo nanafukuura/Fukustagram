b8d7e85445aa241addb5a5d68ad85c15c6a5f4b6
Nana <nana.fukuura@nana.com>
2019-04-20 13:41:50 +0900

    first commit

---
+++ sns/imagenet.py
@@ -0,0 +1,26 @@
+# encoding:utf-8
+
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import numpy as np
+import sys
+
+def imagenet(filename):
+
+  # モデルの設定
+  model = VGG16(weights='imagenet')
+
+  # 入力するデータを読み込み整形
+  img = image.load_img(filename, target_size=(224, 224))
+
+  # 画像を配列に変換
+  x = image.img_to_array(img)
+
+  x = np.expand_dims(x, axis=0)
+
+  # モデルにかけて推論
+  preds = model.predict(preprocess_input(x))
+  results = decode_predictions(preds, top=5)[0]
+
+  # 推論結果を出力
+  return results[0][1]
